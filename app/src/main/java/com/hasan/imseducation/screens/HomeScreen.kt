1eb82e9539300575ccb027cd78d3ce58fe32edb8
PC680 <elshodxushvaqtov07@gmailc.om>
2024-04-03 13:20:17 +0500

    2

--- app/src/main/java/com/hasan/imseducation/screens/HomeScreen.kt
+++ app/src/main/java/com/hasan/imseducation/screens/HomeScreen.kt
@@ -1,13 +1,13 @@
 package com.hasan.imseducation.screens
 
-import android.widget.ImageButton
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,20 +15,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -36,6 +38,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -46,6 +50,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,46 +59,285 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.hasan.imseducation.R
-import com.hasan.imseducation.navigation.Screens
 import com.hasan.imseducation.ui.theme.blue
+import com.hasan.imseducation.ui.theme.blue1
 
 
 @Preview
 @Composable
 fun PreviewHome() {
-   HomeScreen(navController = rememberNavController())
-}
-@Composable
-fun HomeScreen(navController: NavController){
- Column (
-     Modifier
-         .fillMaxSize()
-         .background(Color.White)){
-
-           Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-               Image(
-                   painter = painterResource(id = R.drawable.img_1),
-                   contentDescription = "",
-                   contentScale = ContentScale.Crop,
-                   modifier = Modifier
-                       .padding(vertical = 22.dp, horizontal = 12.dp)
-                       .size(60.dp)
-                       .clip(CircleShape)
-                       .clickable { }
-               )
-               SearchBar()
-               IconButton(onClick = { /*TODO*/ }) {
-                   Image(painter = painterResource(id = R.drawable.img_1), contentDescription = "")
-               }
-           }
-
-   }
-
+    HomeScreen(navController = rememberNavController())
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchBar() {
+fun HomeScreen(navController: NavController) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Spacer(modifier = Modifier.height(6.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.width(8.dp))
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "",
+                modifier = Modifier
+//                    .padding(vertical = 22.dp, horizontal = 12.dp)
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .clickable { },
+                contentScale = ContentScale.Crop
+            )
+            SearchBar()
+            Icon(
+                painter = painterResource(id = R.drawable.menu_svgrepo_com__2_),
+                contentDescription = "",
+                Modifier.size(40.dp),
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+        }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = blue1,
+            )
+        ) {
+            Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ibrat), contentDescription = "",
+                        Modifier
+                            .size(96.dp)
+                            .clip(
+                                CircleShape
+                            ),
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(text = "Ibrat Farzandlari", fontSize = 16.sp)
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ibrat), contentDescription = "",
+                        Modifier
+                            .size(96.dp)
+                            .clip(
+                                CircleShape
+                            ),
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(text = "Ibrat Farzandlari", fontSize = 16.sp)
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ibrat), contentDescription = "",
+                        Modifier
+                            .size(90.dp)
+                            .clip(
+                                CircleShape
+                            ),
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(text = "Ibrat Farzandlari", fontSize = 16.sp)
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ibrat), contentDescription = "",
+                        Modifier
+                            .size(90.dp)
+                            .clip(
+                                CircleShape
+                            ),
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(text = "Ibrat Farzandlari", fontSize = 16.sp)
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ibrat), contentDescription = "",
+                        Modifier
+                            .size(90.dp)
+                            .clip(
+                                CircleShape
+                            ),
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(text = "Ibrat Farzandlari", fontSize = 16.sp)
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ibrat), contentDescription = "",
+                        Modifier
+                            .size(90.dp)
+                            .clip(
+                                CircleShape
+                            ),
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(text = "Ibrat Farzandlari", fontSize = 16.sp)
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                }
+
+            }
+
+        }
+        Row {
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "To'p o'qtuvchilar",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "",
+                    modifier = Modifier
+//                    .padding(vertical = 22.dp, horizontal = 12.dp)
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .clickable { },
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "J.O'rozboyev", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "",
+                    modifier = Modifier
+//                    .padding(vertical = 22.dp, horizontal = 12.dp)
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .clickable { },
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "J.O'rozboyev", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "",
+                    modifier = Modifier
+//                    .padding(vertical = 22.dp, horizontal = 12.dp)
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .clickable { },
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "J.O'rozboyev", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "",
+                    modifier = Modifier
+//                    .padding(vertical = 22.dp, horizontal = 12.dp)
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .clickable { },
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "J.O'rozboyev", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "",
+                    modifier = Modifier
+//                    .padding(vertical = 22.dp, horizontal = 12.dp)
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .clickable { },
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "J.O'rozboyev", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "",
+                    modifier = Modifier
+//                    .padding(vertical = 22.dp, horizontal = 12.dp)
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .clickable { },
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "J.O'rozboyev", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "",
+                    modifier = Modifier
+//                    .padding(vertical = 22.dp, horizontal = 12.dp)
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .clickable { },
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "J.O'rozboyev", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
+
+
+
+        }
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun RowScope.SearchBar() {
     var query by remember { mutableStateOf("") }
     var isSearchActive by remember { mutableStateOf(false) }
     val keyboardController = LocalSoftwareKeyboardController.current
@@ -104,10 +349,13 @@ fun SearchBar() {
         },
         modifier = Modifier
             .padding(16.dp)
-            .width(327.dp),
+            .weight(1f),
         leadingIcon = {
             Icon(
-                modifier = Modifier.size(30.dp), imageVector = Icons.Default.Search, contentDescription = "Search Icon", tint = Black
+                modifier = Modifier.size(30.dp),
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon",
+                tint = Black
             )
         },
         trailingIcon = {
